186cba8524ef390e58e796645bd0ca26d408be2a
Ahmed Abdelhay <ahmedabdelhay6339@gmail.com>
2022-01-24 02:34:35 +0000

    formatting.

--- polygo.cpp
+++ polygo.cpp
@@ -1792,67 +1792,33 @@ Mesh Polygonise(const FunctionType &expr, float isolevel, const float min[3],
             tells us which vertices are inside of the surface
         */
         int cubeindex = 0;
-        if (grid.val[0] < isolevel)
-          cubeindex |= 1;
-        if (grid.val[1] < isolevel)
-          cubeindex |= 2;
-        if (grid.val[2] < isolevel)
-          cubeindex |= 4;
-        if (grid.val[3] < isolevel)
-          cubeindex |= 8;
-        if (grid.val[4] < isolevel)
-          cubeindex |= 16;
-        if (grid.val[5] < isolevel)
-          cubeindex |= 32;
-        if (grid.val[6] < isolevel)
-          cubeindex |= 64;
-        if (grid.val[7] < isolevel)
-          cubeindex |= 128;
+        if (grid.val[0] < isolevel) cubeindex |= 1;
+        if (grid.val[1] < isolevel) cubeindex |= 2;
+        if (grid.val[2] < isolevel) cubeindex |= 4;
+        if (grid.val[3] < isolevel) cubeindex |= 8;
+        if (grid.val[4] < isolevel) cubeindex |= 16;
+        if (grid.val[5] < isolevel) cubeindex |= 32;
+        if (grid.val[6] < isolevel) cubeindex |= 64;
+        if (grid.val[7] < isolevel) cubeindex |= 128;
 
         /* Cube is entirely in/out of the surface */
-        if (edgeTable[cubeindex] == 0) {
-          continue;
-        }
+        if (edgeTable[cubeindex] == 0) { continue; }
 
         Vec3f vertexList[12] = {};
 
         /* Find the vertices where the surface intersects the cube */
-        if (edgeTable[cubeindex] & 1)
-          vertexList[0] =
-              Lerp(isolevel, grid.p[0], grid.p[1], grid.val[0], grid.val[1]);
-        if (edgeTable[cubeindex] & 2)
-          vertexList[1] =
-              Lerp(isolevel, grid.p[1], grid.p[2], grid.val[1], grid.val[2]);
-        if (edgeTable[cubeindex] & 4)
-          vertexList[2] =
-              Lerp(isolevel, grid.p[2], grid.p[3], grid.val[2], grid.val[3]);
-        if (edgeTable[cubeindex] & 8)
-          vertexList[3] =
-              Lerp(isolevel, grid.p[3], grid.p[0], grid.val[3], grid.val[0]);
-        if (edgeTable[cubeindex] & 16)
-          vertexList[4] =
-              Lerp(isolevel, grid.p[4], grid.p[5], grid.val[4], grid.val[5]);
-        if (edgeTable[cubeindex] & 32)
-          vertexList[5] =
-              Lerp(isolevel, grid.p[5], grid.p[6], grid.val[5], grid.val[6]);
-        if (edgeTable[cubeindex] & 64)
-          vertexList[6] =
-              Lerp(isolevel, grid.p[6], grid.p[7], grid.val[6], grid.val[7]);
-        if (edgeTable[cubeindex] & 128)
-          vertexList[7] =
-              Lerp(isolevel, grid.p[7], grid.p[4], grid.val[7], grid.val[4]);
-        if (edgeTable[cubeindex] & 256)
-          vertexList[8] =
-              Lerp(isolevel, grid.p[0], grid.p[4], grid.val[0], grid.val[4]);
-        if (edgeTable[cubeindex] & 512)
-          vertexList[9] =
-              Lerp(isolevel, grid.p[1], grid.p[5], grid.val[1], grid.val[5]);
-        if (edgeTable[cubeindex] & 1024)
-          vertexList[10] =
-              Lerp(isolevel, grid.p[2], grid.p[6], grid.val[2], grid.val[6]);
-        if (edgeTable[cubeindex] & 2048)
-          vertexList[11] =
-              Lerp(isolevel, grid.p[3], grid.p[7], grid.val[3], grid.val[7]);
+        if (edgeTable[cubeindex] & 1)    vertexList[0]  = Lerp(isolevel, grid.p[0], grid.p[1], grid.val[0], grid.val[1]);
+        if (edgeTable[cubeindex] & 2)    vertexList[1]  = Lerp(isolevel, grid.p[1], grid.p[2], grid.val[1], grid.val[2]);
+        if (edgeTable[cubeindex] & 4)    vertexList[2]  = Lerp(isolevel, grid.p[2], grid.p[3], grid.val[2], grid.val[3]);
+        if (edgeTable[cubeindex] & 8)    vertexList[3]  = Lerp(isolevel, grid.p[3], grid.p[0], grid.val[3], grid.val[0]);
+        if (edgeTable[cubeindex] & 16)   vertexList[4]  = Lerp(isolevel, grid.p[4], grid.p[5], grid.val[4], grid.val[5]);
+        if (edgeTable[cubeindex] & 32)   vertexList[5]  = Lerp(isolevel, grid.p[5], grid.p[6], grid.val[5], grid.val[6]);
+        if (edgeTable[cubeindex] & 64)   vertexList[6]  = Lerp(isolevel, grid.p[6], grid.p[7], grid.val[6], grid.val[7]);
+        if (edgeTable[cubeindex] & 128)  vertexList[7]  = Lerp(isolevel, grid.p[7], grid.p[4], grid.val[7], grid.val[4]);
+        if (edgeTable[cubeindex] & 256)  vertexList[8]  = Lerp(isolevel, grid.p[0], grid.p[4], grid.val[0], grid.val[4]);
+        if (edgeTable[cubeindex] & 512)  vertexList[9]  = Lerp(isolevel, grid.p[1], grid.p[5], grid.val[1], grid.val[5]);
+        if (edgeTable[cubeindex] & 1024) vertexList[10] = Lerp(isolevel, grid.p[2], grid.p[6], grid.val[2], grid.val[6]);
+        if (edgeTable[cubeindex] & 2048) vertexList[11] = Lerp(isolevel, grid.p[3], grid.p[7], grid.val[3], grid.val[7]);
 
         /* Create the triangle */
         for (int i = 0; triTable[cubeindex][i] != -1; i += 3) {
